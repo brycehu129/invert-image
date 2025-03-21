e573227de5171646c7025e102977a5af97bd91b9
bryce <hufrancis96@gmail.com>
2025-03-21 23:25:46 +0800

    Update next.config.js for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -4,8 +4,9 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
-  basePath: '/invert-image'
+  images: {
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
