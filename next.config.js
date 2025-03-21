dc78ae213adda6e221df316008c4421bde7b358f
bryce <hufrancis96@gmail.com>
2025-03-22 01:44:59 +0800

    fix: update sitemap configuration

--- next.config.js
+++ next.config.js
@@ -8,15 +8,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['i.pinimg.com', 'pinterest.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      require('fs').copyFileSync(
-        './public/sitemap.xml',
-        './.next/sitemap.xml'
-      );
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig;
