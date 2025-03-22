ec5bf40b861ebdf098b0b0d42d28f499f6df082d
bryce <hufrancis96@gmail.com>
2025-03-22 18:13:56 +0800

    feat: add Google Analytics tracking

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import Script from 'next/script';
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-7KXS13N78N"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-7KXS13N78N');
+        `}
+      </Script>
+    </>
+  );
+} 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css';
 import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import GoogleAnalytics from './components/GoogleAnalytics';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -32,7 +33,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <GoogleAnalytics />
+        {children}
+      </body>
     </html>
   );
 }
