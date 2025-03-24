564354fede47a8ce643086316fbb3d7936153767
Bryce <45346179+brycehu129@users.noreply.github.com>
2025-03-24 11:08:22 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -8,21 +8,6 @@ const nextConfig = {
     unoptimized: true,
     domains: ['i.pinimg.com', 'pinterest.com'],
   },
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.invertimage.net',
-          },
-        ],
-        destination: 'https://invertimage.net/:path*',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
